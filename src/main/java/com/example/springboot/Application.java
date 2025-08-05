a0bec42e69c78359b372a3d8c5787bee850adb71
April Zingher <105899713+AprilZingher@users.noreply.github.com>
2025-08-05 13:17:51 -0400

    Added Spring Security, still minor bugs to work out

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Application {
@@ -23,5 +25,9 @@ public class Application {
             });
             userRepository.findAll().forEach(System.out::println);
         };
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
